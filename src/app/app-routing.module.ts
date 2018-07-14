93e8596489c24c0c3e9686261c4d69cb6473d328
backslash112 <yangcun@live.com>
2018-07-14 16:30:31 -0500

    feat: Jump to login page if the user haven't logged in

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,18 +1,21 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule, Routes } from '@angular/router';
-import { RegisterComponent } from './register/register.component';
-import { LoginComponent } from './login/login.component';
-import { HomeComponent } from './home/home.component';
+import { RegisterComponent } from './components/register/register.component';
+import { LoginComponent } from './components/login/login.component';
+import { HomeComponent } from './components/home/home.component';
+import { AuthGuard } from './guards/auth.guard';
 
 const routes: Routes = [
- { path: "signup", component:  RegisterComponent },
- { path: "signin", component: LoginComponent },
- { path: "home", component: HomeComponent },
+  { path: "signup", component: RegisterComponent },
+  { path: "register", component: RegisterComponent },
+  { path: "signin", component: LoginComponent },
+  { path: "login", component: LoginComponent },
+  { path: "", component: HomeComponent, canActivate: [AuthGuard] },
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [ RouterModule ]
+  exports: [RouterModule]
 })
 export class AppRoutingModule { }
---
+++ src/app/guards/auth.guard.ts
@@ -0,0 +1,31 @@
+import { Injectable } from '@angular/core';
+import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, Router } from '@angular/router';
+import { Observable } from 'rxjs';
+import { take, map } from 'rxjs/operators';
+import { AuthService } from '../services/auth.service';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthGuard implements CanActivate {
+
+  constructor(
+    private authService: AuthService,
+    private router: Router) { }
+
+  canActivate(
+    next: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot): Observable<boolean> | Promise<boolean> | boolean {
+    return this.authService.isLoggedIn
+      .pipe(
+        take(1),
+        map((isLoggedIn: boolean) => {
+          console.log('isLoggedIn: ' + isLoggedIn);
+          if (!isLoggedIn) {
+            this.router.navigate(["login"]);
+            return false;
+          }
+          return true;
+        }))
+  }
+}
