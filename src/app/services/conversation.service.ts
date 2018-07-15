5d62d524e313254a8c91501858a97e8a23bc77d9
backslash112 <yangcun@live.com>
2018-07-15 18:20:44 -0500

    feat: Add conversation service

---
+++ src/app/services/conversation.service.ts
@@ -0,0 +1,74 @@
+import { Injectable, EventEmitter } from '@angular/core';
+import { Conversation } from '../models/conversation.model';
+import { User } from '../models/user.model';
+import { Message } from '../models/message.model';
+import { BehaviorSubject } from 'rxjs';
+import { ChatService } from './chat.service';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ConversationService {
+
+  private _conversationList: Array<Conversation> = new Array();
+  private _currentConversation: Conversation;
+
+  private _conversationListChanged = new BehaviorSubject<Array<Conversation>>(this._conversationList);
+  private _conversationChanged = new BehaviorSubject<Conversation>(this._currentConversation);
+  public conversationChanged$ = this._conversationChanged.asObservable();
+  public conversationListChanged$ = this._conversationListChanged.asObservable();
+
+  constructor(private chatService: ChatService) { 
+    this._conversationList.push(new Conversation(new User("Lucy")));
+    this._conversationList.push(new Conversation(new User("Hannah")));
+    this._conversationList.push(new Conversation(new User("User1")));
+    this._conversationList.push(new Conversation(new User("Meng")));
+    this._conversationListChanged.next(this._conversationList);
+
+    this._currentConversation = this._conversationList[0];
+    this._conversationChanged.next(this._currentConversation);
+
+    this.chatService.onMessage().subscribe(message => {
+      this._currentConversation.pushHistory(message);
+    });
+  }
+
+  public switchConversation(c: Conversation) {
+    this._currentConversation = c;
+    this._conversationChanged.next(c);
+  }
+  
+  public get conversationList(): Array<Conversation> {
+    return this._conversationList;
+  }
+
+  public addNewConversation(c: Conversation) {
+    this._conversationList.push(c);
+  }
+
+  public removeConversation(c: Conversation) {
+    for (let key in this._conversationList) {
+      if (this._conversationList[key].user.email == c.user.email) {
+        delete this._conversationList[key];
+      }
+    }
+  }
+
+  public say(message: Message) {
+    this.chatService.send(message);
+    this._currentConversation.pushHistory(message);
+  }
+
+  public getConversationHistory(): Array<Message> {
+    return this._currentConversation.history;
+  }
+
+  public saveConversationDraft(draft: string){
+    this._currentConversation.draft = draft;
+  }
+
+  public getConversationDraft(): string {
+    return this._currentConversation.draft;
+  }
+
+}
