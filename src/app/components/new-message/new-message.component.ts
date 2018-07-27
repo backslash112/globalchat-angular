17ddbd2c474aef174c8918c0327829662187b804
backslash112 <yangcun@live.com>
2018-07-27 12:20:34 -0500

    feat: Style the new message panel

--- src/app/components/new-message/new-message.component.ts
+++ src/app/components/new-message/new-message.component.ts
@@ -4,6 +4,7 @@ import { ConversationService } from '../../services/conversation.service';
 import { ChatService } from '../../services/chat.service';
 import { AuthService } from '../../services/auth.service';
 import { Message } from '../../models/message.model';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-new-message',
@@ -19,29 +20,41 @@ export class NewMessageComponent implements OnInit {
 
   conversation: Conversation;
   subscription: any;
-  message: string = "";
+  // message: string = "";
+  form: FormGroup;
+  enableInput: boolean = false;
   constructor(
     private conversationService: ConversationService,
-    private authService: AuthService) {
+    private authService: AuthService,
+    private fb: FormBuilder) {
     this.subscription = this.conversationService.conversationChanged$.subscribe(c => {
       if (!c) return;
       this.conversation = c;
-      this.message = c.draft;
+      if (c.draft) {
+        this.form.setValue({ message: c.draft });
+      }
+      this.enableInput = true;
     })
   }
 
   ngOnInit() {
+    this.form = this.fb.group({
+      message: ["", [Validators.required]]
+    })
+    this.form.valueChanges.subscribe(v => console.log(this.form));
+
   }
 
-  onSend() {
-    console.log(this.message)
-    const message = new Message(this.message, this.authService.getCurrentUser(), this.conversation.user);
+  onSubmit() {
+    const messageText = this.form.controls["message"].value;
+    const message = new Message(messageText, this.authService.getCurrentUser(), this.conversation.user);
     this.conversationService.say(message);
-    this.message = "";
+    // messageText = "";
+    this.form.setValue({ message: "" });
     this.conversationService.saveConversationDraft("");
   }
 
   valueChanged() {
-    this.conversationService.saveConversationDraft(this.message);
+    this.conversationService.saveConversationDraft(this.form.controls["message"].value);
   }
 }
